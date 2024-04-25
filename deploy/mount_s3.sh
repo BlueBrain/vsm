b5387e8d1729a06a907d5a4b951a7191f97098ce
acfleury <adrien.fleury@epfl.ch>
2024-04-25 16:03:31 +0200

    Fix missing entrypoint.

--- deploy/mount_s3.sh
+++ deploy/mount_s3.sh
@@ -3,3 +3,6 @@ echo "Behold, fuse mounting S3"
 mkdir -p ${FUSE_MOUNT_POINT}
 /usr/bin/s3fs ${S3_BUCKET_PATH} ${FUSE_MOUNT_POINT} -o iam_role=viz_brayns-ecsTaskRole -o ecs
 ls  /sbo/data/project
+
+echo Passed arguments: "$*"
+eval "$*"
